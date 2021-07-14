b24aab32735e0a52be4b2ff0e34c4cd57a19b8ac
Daphne Song <85268906+daphne1014@users.noreply.github.com>
2021-07-14 13:14:08 -0700

    capture data returned from API

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,7 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-  };
-  
-  getUserRepos();
+let response =
+fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+  response.json().then(function(data) {
+    console.log(data);
+  });
+});
+console.log("outside");
