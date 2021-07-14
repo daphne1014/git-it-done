ca5eea60bd62d6c79b7d1a8679b22fc65d2ae9e7
Daphne Song <85268906+daphne1014@users.noreply.github.com>
2021-07-14 13:25:28 -0700

    update request logic with dynamic variables

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,10 @@
-let response =
-fetch("https://api.github.com/users/octocat/repos").then(function(response) {
-  response.json().then(function(data) {
-    console.log(data);
-  });
-});
-console.log("outside");
+let getUserRepos = function(user) {
+  let apiUrl = "https://api.github.com/users/" + user + "/repos";
+  fetch(apiUrl).then (function(response){
+    response.json(). then(function(data){
+      console.log(data);
+    });
+  }
+  );
+};
+getUserRepos("facebook");
