07a844ffffdd249b11b18d8d027e956ca8048c89
Daphne Song <85268906+daphne1014@users.noreply.github.com>
2021-07-15 15:24:23 -0700

    handle-form-submission

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -7,3 +7,20 @@ let getUserRepos = function(user) {
   }
   );
 };
+
+let userFormEl = document.querySelector("#user-form");
+let nameInputEl = document.querySelector("#username");
+
+let formSubmitHandler = function(event){
+  event.preventDefault();
+  let username = nameInputEl.value.trim();
+
+  if (username){
+    getUserRepos(username);
+    nameInputEl.value="";
+  } else {
+    alert("Please enter a GitHub username");
+  }
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
