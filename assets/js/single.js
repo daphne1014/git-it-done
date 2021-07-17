1e93629634a68c222a64ac262c59fffd781f33fa
Daphne Song <85268906+daphne1014@users.noreply.github.com>
2021-07-16 18:56:54 -0700

    convert-fetched-data-into-dom-elements

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,15 +1,43 @@
+let issueContainerEl = document.querySelector("#issues-container");
+
 let getRepoIssues = function(repo) {
     let apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
     console.log(repo);
     fetch(apiUrl).then(function(response){
         if (response.ok){
-            response.json().then(function(data)
-            {console.log(data);
+            response.json().then(function(data){
+                displayIssues(data);
             });
         }
         else{alert("There was a problem with your resuests!");
     }
     })};
 
+let displayIssues = function(issues){
+    if (issues.length === 0) {
+        issueContainerEl.textContent = "This repo has no open issues!";
+        return;
+      }
+    for (let i=0; i<issues.length;i++){
+        let issueEl = document.createElement ("a");
+        issueEl.classList = "list-item flex-row justify-space-between align-center";
+        issueEl.setAttribute("href", issues[i].html_url);
+        issueEl.setAttribute("target", "_blank");
 
-getRepoIssues("facebook/react");
+        let titleEl=document.createElement("span");
+        titleEl.textContent=issues[i].title;
+
+        issueEl.appendChild(titleEl);
+
+        let typeEl=document.createElement("span");
+
+        if (issues[i].pull_request){
+            typeEl.textContent = "(Pull request)";
+        } else {
+            typeEl.textContent = "(Issue)";
+        }
+        issueEl.appendChild(typeEl);
+
+        issueContainerEl.appendChild(issueEl);
+    }
+}
